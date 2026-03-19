38f55b77f636891e8395313715aa86bdaae8e45a
Chris Poldervaart <cpo@CPOs-MBP.local>
2026-03-19 16:35:01 -0400

    initial upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://blasterkraft.com',
+  site: 'https://o3djeeps.com',
   integrations: [sitemap()],
 });
