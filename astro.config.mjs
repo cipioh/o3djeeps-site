b67c8a3ed7c1e0ae1d55ff1e28d5d242538feffd
Chris Poldervaart <cpo@CPOs-MBP.local>
2026-03-18 21:09:53 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: "https://blasterkraft.com",
+  site: 'https://blasterkraft.com',
+  integrations: [sitemap()],
 });
